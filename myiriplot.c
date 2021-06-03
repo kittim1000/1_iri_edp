ec4b8e684b8034951c3e61f44f6ac463c5aa3daa
kittim1000 <84755321+kittim1000@users.noreply.github.com>
2021-06-03 11:14:11 -0400

    Update myiriplot.c

--- myiriplot.c
+++ myiriplot.c
@@ -1,39 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-void main()
+void main(int argc, char* argv[])
 {
-  int i, jm, iy, imd, iut;
-  float xlat, xlon, hour, vbeg, vend, vstp;
+  int i, j;
 
-  char * gnuplotcmd[] = {
+  char *gnuplotcmd[] = {
     "set terminal png",
     "set output 'EDP.png'",
     "set xrange [0:600000]",
     "set xlabel 'Electron Density Ne/cm-3'",
     "set ylabel 'Altitude (km)'",
-    "set key off",
-    "plot 'gnuplottmp.txt' using 2:1 with lines lc 3 lw 3",
   };
+  char gnuplotcmd2[500] = "plot '1' using 2:1 with lines lc 1 lw 3";
 
-  printf("\n>> jmag(=0/1,geog/geom),lati/deg,long/deg\n");
-  scanf("%d %f %f", &jm, &xlat, &xlon);
+  char str[50], nums[2];
+  FILE *fp, *fp2;
 
-  printf(">> year(yyyy),mmdd(or -ddd),iut(=0/1,LT/UT),hour\n");
-  scanf("%d %d %d %f", &iy, &imd, &iut, &hour);
+  printf("Computing ...\n");
+  sprintf(str, "./iric < %s | grep -v -e '[eo]' > gnuplot.dat", argv[1]);
+  system(str);
 
-  printf(">> begin, end, and stepsize for the selected variable\n");
-  scanf("%f %f %f", &vbeg, &vend, &vstp);
+  fp = fopen("gnuplot.dat", "r");
+
+  i = 0; // Is the input line empty?
+  j = 1;
+
+  while ( fgets (str, 50, fp) != NULL) {
+    if (i == 0 && strlen(str) > 1) {
+      sprintf(nums, "%d", j++);
+      fp2 = fopen(nums, "w");
+      i = 1;
+    }
+
+    if (strlen(str) > 1) fprintf(fp2, "%s", str);
+
+    if (i == 1 && strlen(str) == 1) {
+      fclose (fp2);
+      i = 0;
+    }
+  }
+  fclose (fp);
+
+  for (i=2; i < j; i++) {
+    sprintf(str, ", '%d' using 2:1 with lines lc %d lw 3", i, i);
+    strcat(gnuplotcmd2, str);
+  }
 
   printf("Plotting ...\n");
 
-  FILE * fp = fopen("gnuplottmp", "w");
-  fprintf(fp, "%d %.2f %.2f\n%d %04d %d %.2f\n%.2f %.2f %.2f\n0\n", jm, xlat, xlon, iy, imd, iut, hour, vbeg, vend, vstp); // Print input file for ./iric
-  fclose (fp);
-
-  system("./iric < gnuplottmp | grep -v -e '[eo]' > gnuplottmp.txt"); // Run ./iric
-
   FILE * gnuplotpipe = popen ("gnuplot", "w");
-  for (i=0; i<7; i++) fprintf(gnuplotpipe, "%s \n", gnuplotcmd[i]); // Run gnuplot
+  for (i=0; i < 5; i++) fprintf(gnuplotpipe, "%s \n", gnuplotcmd[i]);
+  fprintf(gnuplotpipe, "%s \n", gnuplotcmd2);
+
   pclose (gnuplotpipe);
 }
