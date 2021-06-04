22e2fe8d8db0c4fb412c1150bc4623c5eeeb9edd
kittim1000 <84755321+kittim1000@users.noreply.github.com>
2021-06-04 10:53:24 -0400

    Update myiriplot.c

--- myiriplot.c
+++ myiriplot.c
@@ -11,44 +11,24 @@ void main(int argc, char* argv[])
     "set xrange [0:600000]",
     "set xlabel 'Electron Density Ne/cm-3'",
     "set ylabel 'Altitude (km)'",
-    "plot '1' u 2:1 with lines lc 1 lw 3 title '1'\\"
+    "plot 'gnuplot.dat' u 2:1 every :::0::0 with lines lc 0 lw 3 title '0'\\"
   };
+  char str[100];
 
-  char str[50], nums[2];
-  FILE *fp, *fp2;
-
-  printf("Computing ...\n");
-  sprintf(str, "./iric < %s | grep -v -e '[eo]' > gnuplot.dat", argv[1]);
+  sprintf(str, "./iric < %s | grep -v -e '[eo]' | tr '\\n' 'Z' | sed -r 's/ZZ+/ZZ/g' | sed 's/Z/\\n/g' > gnuplot.dat", argv[1]);
   system(str);
 
-  fp = fopen("gnuplot.dat", "r");
+  FILE * fp = fopen(argv[1], "r");
 
-  i = 0; // Is the input line empty?
-  j = 1;
-
-  while ( fgets (str, 50, fp) != NULL) {
-
-    if (i == 0 && strlen(str) > 1) {
-      sprintf(nums, "%d", j++);
-      fp2 = fopen(nums, "w");
-      i = 1;
-    }
-
-    if (strlen(str) > 1) fprintf(fp2, "%s", str);
-
-    if (i == 1 && strlen(str) == 1) {
-      fclose (fp2);
-      i = 0;
-    }
-  }
-  fclose (fp);
-
-  printf("Plotting ...\n");
+  j = 0;
+  while ( fgets (str, 100, fp) != NULL)
+    if (strlen(str) > 1) j++;
 
   FILE * gnuplotpipe = popen ("gnuplot", "w");
   for (i=0; i < 6; i++) fprintf(gnuplotpipe, "%s\n", gnuplotcmd[i]);
-  for (i=2; i < j; i++) fprintf(gnuplotpipe, ", '%d' u 2:1 with lines lc %d lw 3 title '%d'\\\n", i, i, i);
+  for (i=1; i < j; i++) fprintf(gnuplotpipe, ", 'gnuplot.dat' u 2:1 every :::%d::%d with lines lc %d lw 3 title '%d'\\\n", i, i, i, i);
   fprintf(gnuplotpipe, "\n");
 
+  fclose (fp);
   pclose (gnuplotpipe);
 }
